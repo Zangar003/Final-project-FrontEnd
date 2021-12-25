d10ecf9e1acc3d951943d37975fd1df49cd874d0
Zangar <200103376@stu.sdu.edu.kz>
2021-12-25 08:48:45 +0600

    Final Project

---
+++ Password/app.js
@@ -0,0 +1,12 @@
+VANTA.GLOBE({
+  el: "#intro",
+  mouseControls: true,
+  touchControls: true,
+  gyroControls: false,
+  minHeight: 200.00,
+  minWidth: 200.00,
+  scale: 1.00,
+  scaleMobile: 1.00,
+  color: 0x8b49b1,
+  backgroundColor: 0x50509
+})
